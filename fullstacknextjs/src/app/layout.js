ec9eb7037ba551187bff6aeec062453255d10a06
shankerpangeni <shanker.pangeni10@gmail.com>
2025-11-26 09:52:34 +0545

    scroll fixed

--- fullstacknextjs/src/app/layout.js
+++ fullstacknextjs/src/app/layout.js
@@ -5,6 +5,8 @@ import "react-toastify/dist/ReactToastify.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
+import ScrollToTop from "@/components/ScrollToTop";
+
 export const metadata = {
   title: "Movie Booking System",
   description: "Book your favorite movies online",
@@ -15,6 +17,7 @@ export default function RootLayout({ children }) {
     <html lang="en" suppressHydrationWarning>
       <body className="bg-gray-900 text-white min-h-screen flex flex-col" suppressHydrationWarning>
         <ReduxProvider>
+          <ScrollToTop />
           <Navbar />
           <main className="container mx-auto px-4 py-8 mt-16 flex-1">
             {children}
